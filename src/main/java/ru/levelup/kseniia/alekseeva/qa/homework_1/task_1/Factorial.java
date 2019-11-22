38ef1a44c1190edfec5dd88ad412944941ff3807
kaleksee <xusha.alexeeva@mail.ru>
2019-11-22 19:10:43 +0300

    commit homework1

---
+++ src/main/java/ru/levelup/kseniia/alekseeva/qa/homework_1/task_1/Factorial.java
@@ -0,0 +1,33 @@
+package ru.levelup.kseniia.alekseeva.qa.homework_1.task_1;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Factorial {
+
+    public long calculateFactorialRecursion() {
+        try {
+            System.out.println("Input int A");
+            Scanner input = new Scanner(System.in);
+            int variableA = input.nextInt();
+            return doRecursion(variableA);
+
+
+        } catch (InputMismatchException any) {
+            throw new RuntimeException("Please check the input! Int expected!");
+
+        }
+    }
+
+    private long doRecursion(int a) {
+        if (a == 0) {
+            return 1;
+
+        } else {
+            return (a * doRecursion(a - 1));
+        }
+
+    }
+}
+
+
---
+++ src/main/java/ru/levelup/kseniia/alekseeva/qa/homework_1/task_1/Substraction.java
@@ -0,0 +1,27 @@
+package ru.levelup.kseniia.alekseeva.qa.homework_1.task_1;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Substraction {
+    public int substractTwoNumbers() {
+        try {
+            System.out.println("Input integer A");
+            Scanner input = new Scanner(System.in);
+            int variableA = input.nextInt();
+            System.out.println("Input integer B");
+            int variableB = input.nextInt();
+            return (variableA - variableB);
+        }
+        catch (InputMismatchException any) {
+            throw new RuntimeException("Please check the input! Int expected!");
+        }
+
+    }
+
+    public void testMethod () {
+
+    }
+
+
+}
